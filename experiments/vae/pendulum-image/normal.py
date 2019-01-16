5641ad116f8a16eaf1ed300242ccf5f7941d44eb
Sharad Vikram <sharad.vikram@gmail.com>
2019-01-15 17:15:53 -0800

    latest pendulum

--- experiments/vae/pendulum-image/normal.py
+++ experiments/vae/pendulum-image/normal.py
@@ -15,7 +15,7 @@ du = da = env.get_action_dim()
 horizon = 200
 
 experiment = dict(
-    experiment_name='pendulum-image',
+    experiment_name='pendulum-image-blds-smooth',
     experiment_type='train_vae',
     env=env_params,
     model=dict(
@@ -30,28 +30,31 @@ experiment = dict(
                     >> nn.Flatten() >> nn.Bernoulli()),
         action_encoder=nn.IdentityVariance(variance=1e-4),
         action_decoder=nn.IdentityVariance(variance=1e-4),
-        prior=dict(prior_type='lds', smooth=True),
+        prior=sweep([
+            dict(prior_type='blds', smooth=True),
+            dict(prior_type='blds', smooth=False),
+        ], ['blds-smooth', 'blds']),
     ),
     train=dict(
         num_epochs=1000,
         learning_rate=1e-3,
-        model_learning_rate=1e-3,
+        model_learning_rate=1e-5 / 4,
         beta_start=1e-4,
         beta_end=10.0,
         beta_rate=5e-5,
         beta_increase=0,
-        batch_size=1,
+        batch_size=2,
         dump_every=100,
-        summary_every=10,
+        summary_every=50,
     ),
     data=dict(
-        # load_data=Path('out/vae/pendulum/rollouts.pkl'),
+        load_data=Path('s3://parasol-experiments/data/pendulum-200.pkl'),
         num_rollouts=100,
         init_std=2.0,
-        smooth_noise=True,
+        smooth_noise=False,
     ),
     dump_data=True,
     seed=0,
-    out_dir='out/vae/pendulum',
+    out_dir='s3://parasol-experiments/vae/pendulum-image',
 )
-run(experiment, remote=False, gpu=True, num_threads=1, instance_type='g3.4xlarge')
+run(experiment, remote=True, gpu=False, num_threads=1, instance_type='m5.4xlarge')
